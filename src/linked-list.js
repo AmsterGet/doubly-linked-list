b519ae682f7967d196e10f0aae487c4c492c5a8d
AmsterGet <amsterget@gmail.com>
2017-09-29 19:00:55 +0300

    Eleven of 15

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,91 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._head=null;
+        this._tail=null;
+        this.length=0;
+    }
 
-    append(data) {}
+    append(data) {
+        var node = new Node(data);
+        if (this.length) {
+            node.prev = this._tail;
+            this._tail.next = node;
+            this._tail = node;
+        } else {
+            this._head = node;
+            this._tail = node;
+        }
+        this.length++;
+    }
 
-    head() {}
+    head() {
+        return this._head.data;
+    }
 
-    tail() {}
+    tail() {
+        return this._tail.data;
+    }
 
-    at(index) {}
+    at(index) {
+        var i,
+            x=this._head;
+        while (index>0){
+            x=x.next;
+            index--;
+        }
+        return x.data;
+    }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
 
-    isEmpty() {}
+    }
 
-    clear() {}
+    isEmpty() {
+        return (!this.length);
+    }
 
-    deleteAt(index) {}
+    clear() {
+        this._head.data=null;
+        this._head.next=null;
+        this._head.prev=null;
+        this._tail.data=null;
+        this._tail.next=null;
+        this._tail.prev=null;
+        this.length=0;
+    }
 
-    reverse() {}
+    deleteAt(index) {
 
-    indexOf(data) {}
+    }
+
+    reverse() {
+        var x=this._tail,i,buf;
+        this._head=x;
+        for(i=0;i<this.length;i++){
+            buf=x.prev;
+            x.prev=x.next;
+            x.next=buf;
+            if(x.next){
+                x=x.next;
+            }
+            else break;
+        }
+        this._tail=x;
+    }
+
+    indexOf(data) {
+        var i=0,
+            x=this._head;
+        while (i<this.length){
+            if (x.data===data) break;
+            x=x.next;
+            i++;
+        }
+        if (i===this.length) return -1;
+        return i;
+    }
 }
 
 module.exports = LinkedList;
